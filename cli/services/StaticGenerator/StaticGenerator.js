7af0babaa7daaa4209915ef93c6546d94370a088
Troy Forster <troy.forster@gmail.com>
2019-09-16 17:37:03 -0400

    New architecture! (#13)
    * WIP for Jake to review if can before he leaves
    
    * Significant refactoring greatly improves the architecture for adding new commands
    - All previous functionality temporarily stripped out with the exception of "build static"
    - Original functionality to be re-introduced feature by feature

--- cli/services/StaticGenerator/StaticGenerator.js
+++ cli/services/StaticGenerator/StaticGenerator.js
@@ -1,30 +1,48 @@
 'use strict';
 
+// Node system dependencies
+const fs = require('fs');
+const path = require('path');
+
+// StaticGenerator dependencies
 const babili = require('gulp-babili');
 const debug = require('gulp-debug');
 const del = require('del');
-const ejs = require('gulp-ejs');
 const gulp = require('gulp');
 const htmlmin = require('gulp-htmlmin');
 const minifyCss = require('gulp-minify-css');
-const path = require('path');
 const rename = require('gulp-rename');
 const rev = require('gulp-rev');
 const through = require('through2');
-const url = require('url');
 const usemin = require('gulp-usemin');
 
-class StaticGenerator {
-  constructor(config, options, renderModule) {
-    this.config = config;
-    config.src = './src';
-    this.stage = options.stage;
-    this.renderModule = renderModule;
 
-    // Joy says data should be in _generator/data.json
-    //    this.payload = require(path.join(process.cwd(), config.src, '_generator/data.json'));
-    // Attach the renderer function
-    //  this.payload.renderer = this.renderer;
+/**
+ * Mostly template-agnostic static site generator 
+ * @class StaticGenerator
+ */
+class StaticGenerator {
+
+
+  /**
+   * Creates an instance of StaticGenerator.
+   * @param {class} joy An instance of the Joy framework
+   * @param {object} args
+   * @memberof StaticGenerator
+   */
+  constructor(joy, args) {
+    this.args = args;
+    this.joy = joy;
+    this.joy.config.src = path.join(this.joy.config.projectRoot, './src');
+    this.joy.config.stage = args.flags.stage;
+    this.config = joy.config;
+    this.stage = args.flags.stage.value;
+
+    // ! HACK: Had to tack stage on to renderData to get Jake's to work. Needs elegance++
+    const renderData = require(path.join(this.joy.config.projectRoot, './src/_generator/data.json'));
+    renderData.stage = this.stage;
+
+    this.renderModule = new (require(path.join(this.joy.config.projectRoot, './src/_generator/renderer.js')))(renderData);
 
     // Set build path
     switch (this.stage.toLowerCase()) {
@@ -39,56 +57,62 @@ class StaticGenerator {
         break;
     }
 
+    this.buildPath = path.join(this.joy.config.projectRoot, this.buildPath);
+
+    // TODO: Re-implement support for markdown-to-HTML
     // this.config = {
     // }
     // this.marked = marked;
     // this.marked.setOptions(this.config.marked);
+
+    // TODO: Re-implement support for calling Slack webhook notifications
     // this.slack = require('gulp-slack')(this.config.slack);
+
+    // TODO: Implement a -v, --verbose flag for output like the following
     console.log(`Using target: ${this.stage} from ${this.config.src} to ${this.buildPath}`);
   }
 
   /**
    * Constructs .html views from .ejs templates in src/views
-   *
    * @returns
    * @memberof Tasks
    */
-  _compileViews() {
-    const self = this;
-    return new Promise((resolve, reject) => {
-      // See https://github.com/mde/ejs for options
-      // TODO: replace gulp external dependency with simple vinyl-fs function
+  async _compileViews() {
+    // See https://github.com/mde/ejs for options
+    // TODO: replace gulp external dependency with simple vinyl-fs function
 
-      gulp
-        .src(`${this.config.src}/_views/**/*.html`)
+    const dest = path.join(this.buildPath);
 
-        // Setup error and end handlers
-        .on('error', (e) => {
-          console.error('_compileViews:', e);
-          reject(e);
+
+    gulp
+      .src(`${this.config.src}/_views/**/*.html`)
+
+      // Setup error handler
+      .on('error', (e) => {
+        console.error('_compileViews:', e);
+      })
+
+      // Pipe stream to the various stages
+      .pipe(debug())
+
+      // TODO: Must be renderer agnostic. RenderModule to export generic render function so we don't have this.renderModule.ejs
+      .pipe(this.renderModule.ejs({ d: this.renderModule }))
+
+      // Remove folders and files starting with _ from the stream
+      .pipe(
+        through.obj((file, enc, cb) => {
+          if (path.parse(file.path).name.indexOf('_') !== 0) {
+            cb(null, file);
+          } else {
+            cb();
+          }
         })
-        .on('end', resolve)
+      )
 
-        // Pipe stream to the various stages
-        .pipe(debug())
-
-        // Pipe to ejs
-        // TODO: Replace ejs with call to this.renderModule.renderLib
-        .pipe(ejs({ d: this.renderModule }))
-
-        // Filter _private files out of the stream to dest
-        .pipe(
-          through.obj((file, enc, cb) => {
-            if (path.parse(file.path).name.indexOf('_') !== 0) {
-              cb(null, file);
-            } else {
-              cb();
-            }
-          })
-        )
-        .pipe(gulp.dest(path.join(`${this.config.projectRoot}`, this.buildPath)));
-    });
+      // Save to the build path
+      .pipe(gulp.dest(dest));
   }
+
 
   /**
    * Compiles views and copies, including resources, to build path with optional concatenation and minification
@@ -96,11 +120,13 @@ class StaticGenerator {
    * @param {*} dev : If dev = true then don't minify/concatenate
    */
   async build(dev = true) {
-    await del([`${this.buildPath}**/*`]);
+    // Prepare the build destination for a fresh build 
+    // ! NOTE: rmdirSync recursive requires node 12.10.0 minimum
+    fs.rmdirSync(this.buildPath, { recursive: true });
+    fs.mkdirSync(this.buildPath);
 
-    await this._compileViews().catch((e) => {
-      console.error('caught1', e);
-    });
+    // Compile the views
+    const r = await this._compileViews();
 
     await this._copyResources([
       {
@@ -145,7 +171,9 @@ class StaticGenerator {
 
     if (!dev) {
       // Pass through the minification and concatenation stream
-      await this._minh();
+      await this._minh().catch((reason) => {
+        throw reason;
+      })
       // Remove redundant files copied to the output
       await del([`${this.buildPath}/js`, `${this.buildPath}/css`]);
     }
@@ -190,12 +218,15 @@ class StaticGenerator {
                 }
               })
             )
-            .pipe(gulp.dest(this.buildPath + resource.dest))
+            .pipe(gulp.dest(path.join(this.buildPath, resource.dest)))
             .on('end', resolve);
         });
       })
     );
   }
+
+
+  // TODO: Figure out why this code throws a PluginError
 
   /**
    * Minifies and concatenates globbed .HTML files including nested Javascript and CSS resources
@@ -204,37 +235,41 @@ class StaticGenerator {
    * @memberof Tasks
    */
   _minh() {
-    const self = this;
+    const src = this.buildPath + '**/*.html';
+    const dest = this.buildPath;
+
     return new Promise((resolve, reject) => {
-      console.log('build path:', self.buildPath);
       gulp
-        .src(`${self.buildPath}/**/*.html`)
+        .src(src)
         .pipe(debug())
-        .on('error', reject)
+        .on('error', (err) => {
+          console.error('_minh', err);
+          return reject();
+        })
         .pipe(
           usemin({
             css: [
-              function() {
+              function () {
                 return minifyCss();
               },
-              function() {
+              function () {
                 return rev();
               }
             ],
             html: [
-              function() {
+              function () {
                 return htmlmin({ collapseWhitespace: true, removeComments: true });
               }
             ],
             js: [
-              function() {
+              function () {
                 return babili({
                   mangle: {
                     keepClassNames: true
                   }
                 });
               },
-              function() {
+              function () {
                 return rev();
               }
             ],
@@ -245,11 +280,15 @@ class StaticGenerator {
                 }
               })
             ],
-            inlinecss: [minifyCss(), 'concat']
+            inlinecss: [minifyCss(), 'concat'],
+            skipMissingResources: true
           })
         )
-        .pipe(gulp.dest(self.buildPath))
-        .on('end', resolve);
+        .pipe(gulp.dest(dest))
+
+        .on('end', () => {
+          return resolve();
+        });
     });
   }
 
