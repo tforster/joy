7c044859712310390855aa84a7eb8c2c760b11e7
Troy Forster <troy.forster@gmail.com>
2020-12-23 12:59:42 -0500

    Fixes AB#843 Create shell script to automate S3 bucket creation

---
+++ scripts/aws-s3-new-web.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Creates a new S3 bucket configured as a website using extensionless defaults of index and error
+#
+# @param {string} bucket as $1:   The unique bucket name
+# @param {string} region as $2:   The region to create the bucket in
+# @param {string} profile as $3:  The ~/.aws/credentials profile name to use
+
+# Turn off the default pager which causes the script to pause after each command
+export AWS_PAGER=""
+
+# Create the temporary bucket policy json file 
+echo '{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "s3:GetObject",
+            "Resource": "arn:aws:s3:::'$1'/*"
+        }
+    ]
+}' > /tmp/bucket_policy.json
+
+# Create the bucket named $1 in the region $2 using profile $3
+aws s3api create-bucket --bucket $1 --region $2  --create-bucket-configuration LocationConstraint=$2 --profile $3    
+
+# Set the bucket policy created above
+aws s3api put-bucket-policy --bucket $1 --policy file:///tmp/bucket_policy.json --profile $3    
+
+# Configure the bucket as a website using index and error as the two default fuiles
+aws s3 website s3://$1/ --index-document index --error-document error --profile $3   
